9659be379d14e1365e9ba94760a6b0a3d8163720
tbehringer <tomas.behringer@mercadolibre.com>
2021-05-16 04:13:10 -0300

    IDogsApi.kt: change method to pass Breed in query

--- app/src/main/java/com/utn/MascotApp/IDogsApi.kt
+++ app/src/main/java/com/utn/MascotApp/IDogsApi.kt
@@ -1,9 +1,10 @@
 package com.utn.MascotApp
 
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Url
 
 interface IDogsApi {
-    @GET("random")
-    fun get_dog_image(): Call<Mascotas>
+    @GET
+    suspend fun get_dogs_image_by_breed(@Url url:String): Response<MascotasResponse>
 }
