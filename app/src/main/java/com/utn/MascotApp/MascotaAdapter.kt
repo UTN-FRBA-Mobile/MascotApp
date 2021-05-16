f1ff12857c4898d464392227597149560cf1fb4f
tbehringer <tomas.behringer@mercadolibre.com>
2021-05-16 04:13:56 -0300

    MascotaAdapter.kt

--- app/src/main/java/com/utn/MascotApp/MascotaAdapter.kt
+++ app/src/main/java/com/utn/MascotApp/MascotaAdapter.kt
@@ -4,33 +4,23 @@ import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import com.squareup.picasso.Picasso
 
 
-class MascotaAdapter(private val mascotas: List<Mascota>): RecyclerView.Adapter<MascotaAdapter.ViewHolder>(){
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(viewType, parent, false)
 
-        return ViewHolder(view)
+class MascotaAdapter(private val images: List<String>): RecyclerView.Adapter<TarjetaMascotaHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TarjetaMascotaHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return TarjetaMascotaHolder(layoutInflater.inflate(R.layout.tarjeta_mascota, parent, false))
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val itemIndex = position - 1 // el primer item es el encabezado
-
-        holder.itemView.findViewById<TextView>(R.id.card_title).text = "Perro " + itemIndex.toString()
-        holder.itemView.findViewById<TextView>(R.id.card_secondary_text).text = "Perdido en tal lado"
-
-        // solo si hay imagen usar holder.itemView.image
-        Picasso.get().load(Uri.parse(mascotas[itemIndex].foto_mascota)).into(holder.itemView.findViewById<ImageView>(R.id.card_imagen_mascota))
-        //Picasso.get().load(Uri.parse(mascotas[itemIndex].message)).into(holder.itemView.findViewById<ImageView>(R.id.card_imagen_mascota))
+    override fun onBindViewHolder(holder: TarjetaMascotaHolder, position: Int) {
+        val item = images[position]
+        holder.bind(item)
     }
 
-    override fun getItemCount(): Int = mascotas.size + 1  // el primer item es el encabezado
+    override fun getItemCount(): Int = images.size
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    }
 }
