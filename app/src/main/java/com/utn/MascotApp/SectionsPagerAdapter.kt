f36492e6d4701def3c625226e1d0589d7f7858aa
tbehringer <tomas.behringer@mercadolibre.com>
2021-05-20 23:40:02 -0300

    PageViewModel, PlaceHolderFragment and SectionsPagerAdapter

---
+++ app/src/main/java/com/utn/MascotApp/SectionsPagerAdapter.kt
@@ -0,0 +1,27 @@
+package com.utn.MascotApp
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+private val TAB_TITLES = arrayOf(
+        R.string.tab_listado,
+        R.string.tab_map
+)
+
+class SectionsPagerAdapter(private val context: Context, fm: FragmentManager): FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+        // Show 2 total pages.
+        return 2
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return PlaceholderFragment.newInstance(position + 1)
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return context.resources.getString(TAB_TITLES[position])
+    }
+
+}
