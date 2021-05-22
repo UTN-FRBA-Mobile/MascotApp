1086dee99561f5d2e3a407582c61c2f5389a7c13
tbehringer <tomas.behringer@mercadolibre.com>
2021-05-21 23:04:31 -0300

    PagerViewAdapter: add PagerViewAdapter

--- app/src/main/java/com/utn/MascotApp/PagerViewAdapter.kt
+++ app/src/main/java/com/utn/MascotApp/PagerViewAdapter.kt
@@ -1,17 +1,13 @@
 package com.utn.MascotApp
 
-import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.utn.MascotApp.fragments.MapsFragment
+import com.utn.MascotApp.fragments.MascotaVistasFragment
 
-private val TAB_TITLES = arrayOf(
-        R.string.tab_listado,
-        R.string.tab_map
-)
 
-class SectionsPagerAdapter(private val context: Context, fm: FragmentManager): FragmentPagerAdapter(fm) {
+class PagerViewAdapter(fm: FragmentManager): FragmentPagerAdapter(fm) {
     override fun getCount(): Int {
         // Show 2 total pages.
         return 2
@@ -19,7 +15,6 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager): F
 
     override fun getItem(position: Int): Fragment {
         // check which pages should be destroyed and which should be kept
-//        return PlaceholderFragment.newInstance(position + 1)
         return when(position){
             0 -> {
                 MapsFragment()
@@ -29,10 +24,6 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager): F
             }
             else -> MapsFragment()
         }
-    }
-
-    override fun getPageTitle(position: Int): CharSequence? {
-        return context.resources.getString(TAB_TITLES[position])
     }
 
 }
