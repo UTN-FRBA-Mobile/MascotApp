8b2b5edb3938039c536de8121f98164103a351bb
agustin-rosas-frba <agustinmiguelrosas@frba.utn.edu.ar>
2021-05-24 02:01:24 -0300

    Guardar usuario en sesion

--- app/src/main/java/com/utn/MascotApp/fragments/AuthFragment.kt
+++ app/src/main/java/com/utn/MascotApp/fragments/AuthFragment.kt
@@ -1,5 +1,6 @@
 package com.utn.MascotApp.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +13,7 @@ import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.utn.MascotApp.R
 import com.utn.MascotApp.databinding.FragmentAuthBinding
+
 
 class AuthFragment: Fragment() {
     private var _binding: FragmentAuthBinding? = null
@@ -35,6 +37,15 @@ class AuthFragment: Fragment() {
         email = binding.email
         password = binding.password
         setup()
+        checkSession()
+    }
+
+    private fun checkSession() {
+        val prefs = this.getActivity()?.getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+        val sessionEmail = prefs?.getString("email", null)
+        if (sessionEmail != null) {
+            findNavController().navigate(R.id.mainMenuFragment)
+        }
     }
 
     private fun setup() {
@@ -42,11 +53,13 @@ class AuthFragment: Fragment() {
             if (email.text.isNotEmpty() && password.text.isNotEmpty()) {
                 FirebaseAuth.getInstance().createUserWithEmailAndPassword(email.text.toString(), password.text.toString()).addOnCompleteListener {
                     if (it.isSuccessful) {
-                        navigateHome()
+                        saveSessionAndNavigateHome()
                     } else {
-                        showAlert()
+                        showAlert(it.exception.toString())
                     }
                 }
+            } else {
+                showEmptyValuesAlert()
             }
         }
 
@@ -54,26 +67,43 @@ class AuthFragment: Fragment() {
             if (email.text.isNotEmpty() && password.text.isNotEmpty()) {
                 FirebaseAuth.getInstance().signInWithEmailAndPassword(email.text.toString(), password.text.toString()).addOnCompleteListener {
                     if (it.isSuccessful) {
-                        navigateHome()
+                        saveSessionAndNavigateHome()
                     } else {
-                        showAlert()
+                        showAlert(it.exception.toString())
                     }
                 }
+            } else {
+                showEmptyValuesAlert()
             }
         }
     }
 
-    private fun showAlert() {
+    private fun showAlert(errorMessage: String) {
         val alertBuilder = context?.let {
             AlertDialog.Builder(it)
                 .setTitle("Error")
-                .setMessage("Se ha producido un error autenticando al usuario")
+                .setMessage("Se ha producido un error autenticando al usuario, " + errorMessage)
                 .setPositiveButton("Aceptar", null)
         }
         alertBuilder?.create()?.show()
     }
 
-    private fun navigateHome() {
+    private fun showEmptyValuesAlert() {
+        val alertBuilder = context?.let {
+            AlertDialog.Builder(it)
+                .setTitle("Error")
+                .setMessage("Por favor ingrese un Usuario y Password")
+                .setPositiveButton("Aceptar", null)
+        }
+        alertBuilder?.create()?.show()
+    }
+
+
+    private fun saveSessionAndNavigateHome() {
+
+        val prefs = this.requireActivity().getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.putString("email", email.text.toString())
+        prefs.apply()
         findNavController().navigate(R.id.mainMenuFragment)
     }
 }
