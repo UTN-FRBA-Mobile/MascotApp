2474710f2d62761de8365b9c7b5690dccf2ce470
agustin-rosas-frba <agustinmiguelrosas@frba.utn.edu.ar>
2021-05-30 20:49:48 -0300

    Creacion de modelo, integracion con servicios de db y storage de Firebase

---
+++ app/src/main/java/com/utn/MascotApp/models/Publication.kt
@@ -0,0 +1,44 @@
+package com.utn.MascotApp.models
+
+import com.google.firebase.firestore.Exclude
+import com.google.firebase.firestore.GeoPoint
+import com.google.firebase.firestore.IgnoreExtraProperties
+import java.util.*
+import kotlin.collections.ArrayList
+
+@IgnoreExtraProperties
+data class Publication(
+    var type: String? = "",
+    var species: String? = "",
+    var race: String? = "",
+    var createdAt: Date = Date(),
+    var lastSeen: Date = Date(),
+    var color: String? = "",
+    var size: String? = "",
+    var name: String? = "",
+    var description: String = "",
+    var address: String? = "",
+    var geolocation: GeoPoint = GeoPoint(0.0,0.0),
+    var imagePath: String = "",
+    var createdBy: String? = ""
+) {
+
+    @Exclude
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "type" to type,
+            "species" to species,
+            "race" to race,
+            "createdAt" to createdAt,
+            "lastSeen" to lastSeen,
+            "color" to color,
+            "size" to size,
+            "name" to name,
+            "description" to description,
+            "address" to address,
+            "geolocation" to geolocation,
+            "createdBy" to createdBy,
+            "imagePath" to imagePath,
+        )
+    }
+}
