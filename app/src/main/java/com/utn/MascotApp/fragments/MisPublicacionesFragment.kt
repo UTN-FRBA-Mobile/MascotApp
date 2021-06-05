4a9d9c77266f97638822f9143254661c505bc793
tbehringer <tomas.behringer@mercadolibre.com>
2021-06-05 16:35:52 -0300

    image loadoing

--- app/src/main/java/com/utn/MascotApp/fragments/MisPublicacionesFragment.kt
+++ app/src/main/java/com/utn/MascotApp/fragments/MisPublicacionesFragment.kt
@@ -1,6 +1,7 @@
 package com.utn.MascotApp.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -23,6 +24,7 @@ import com.google.firebase.Timestamp
 
 
 class MisPublicacionesFragment : Fragment() {
+    val DEFAULT_NO_IMAGE = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.lasommeliere.com%2Fes%2F105-.html&psig=AOvVaw30Df0cC5QXK2zJEk-2LgfG&ust=1623004664092000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPD39vyRgfECFQAAAAAdAAAAABAO"
     private val fromBotton: Animation by lazy{ AnimationUtils.loadAnimation(context, R.anim.from_bottom_anim)}
     private val toBotton: Animation by lazy{ AnimationUtils.loadAnimation(context, R.anim.to_bottom_anim)}
     private var _binding: FragmentMisPublicacionesBinding? = null
@@ -109,42 +111,43 @@ class MisPublicacionesFragment : Fragment() {
         var collection_publications : MutableList<Publications> = arrayListOf()
         val db = FirebaseFirestore.getInstance()
         val createdBy = FirebaseAuth.getInstance().currentUser?.uid
+        val storage = Firebase.storage
+        val imagesRef = storage.reference.child("publication-images")
         db.collection("publications")
                 .get()
                 .addOnSuccessListener { documents ->
                     for (document in documents) {
                         var map : Map<String, Any> = document.data
-//                      TODO este if no se si funciona, no me trae solo mis publicaciones......
                         if (createdBy == map.get("createdBy").toString()){
-//                      fin TODO
+                            imagesRef.child(map.get("imagePath").toString()).downloadUrl.addOnSuccessListener {
                                 var publication = Publications(
-                                        address = map.get("address").toString(),
-                                        color = map.get("color").toString(),
-                                        imagePath = map.get("imagePath").toString(),
-//                                      TODO hay que cargar las imágenes de la URL real.
-//                                      dejo esta hardcodeada  imagePath = "https://laughingcolours.com/wp-content/uploads/2019/06/k-s-pets-services-ecil-hyderabad-pet-care-takers-1knoqwn9vh-1.jpg",
-//                                      FIN TODO
-                                        description = map.get("description").toString(),
-                                        type = map.get("type").toString(),
-                                        breed = map.get("breed").toString(),
-                                        createdAt = map.get("createdAt") as Timestamp,
-                                        lastSeen = map.get("lastSeen") as Timestamp,
-                                        size = map.get("size").toString(),
-                                        createdBy = map.get("createdBy").toString(),
-                                        species = map.get("species").toString(),
-                                        name = map.get("name").toString(),
-                                        geolocation = map.get("geolocation") as GeoPoint
+                                    address = map.get("address").toString(),
+                                    color = map.get("color").toString(),
+                                    imagePath = it.toString(),
+                                    description = map.get("description").toString(),
+                                    type = map.get("type").toString(),
+                                    breed = map.get("breed").toString(),
+                                    createdAt = map.get("createdAt") as Timestamp,
+                                    lastSeen = map.get("lastSeen") as Timestamp,
+                                    size = map.get("size").toString(),
+                                    createdBy = map.get("createdBy").toString(),
+                                    species = map.get("species").toString(),
+                                    name = map.get("name").toString(),
+                                    geolocation = map.get("geolocation") as GeoPoint
                                 )
                                 collection_publications.add(publication)
+                                publications.clear()
+                                publications.addAll(collection_publications)
+                                mascotaAdapter.notifyDataSetChanged()
+                                binding.progressBar.visibility = View.GONE
+                            }
                             }
                         }
-                    publications.clear()
-                    publications.addAll(collection_publications)
-                    mascotaAdapter.notifyDataSetChanged()
-                    binding.progressBar.visibility = View.GONE
+
                 }
                 .addOnFailureListener { exception ->
                     println("Error getting documents: $exception")
                 }
+
     }
 }
